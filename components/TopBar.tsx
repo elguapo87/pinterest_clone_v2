161b9a8eed5ed905340b01ac19fc5649d2b325e5
elguapo87 <pglogovac@gmail.com>
2026-06-13 00:21:51 +0200

    Created TopBar & UserButton components

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,9 +1,23 @@
+import Image from "next/image"
+import UserButton from "./UserButton"
+
 const TopBar = () => {
-  return (
-    <div>
-      TopBar
-    </div>
-  )
+    return (
+        <div className="my-4 mx-0 flex items-center gap-4">
+            {/* SEARCH */}
+            <div className="flex-1 bg-[#f1f1f1] rounded-2xl p-4 flex items-center gap-4">
+                <Image src="/search.svg" alt="Search" width={16} height={16} />
+                <input
+                    className="flex-1 bg-transparent border-none outline-none text-[18px]"
+                    type="text"
+                    placeholder="Search..."
+                />
+            </div>
+
+            {/* USER */}
+            <UserButton />
+        </div>
+    )
 }
 
 export default TopBar
--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -1,9 +1,61 @@
+"use client"
+
+import { useClickOutside } from "@/hooks/clickOutside";
+import Image from "next/image";
+import Link from "next/link";
+import { useRef, useState } from "react"
+
 const UserButton = () => {
-  return (
-    <div>
-      UserButton
-    </div>
-  )
+    const currentUser = true;
+
+    const openRef = useRef<HTMLDivElement | null>(null);
+    const [open, setOpen] = useState(false);
+
+    useClickOutside(openRef, () => {
+        setOpen(false);
+    }); 
+
+    return currentUser ? (
+        <div ref={openRef} className="max-sm:hidden relative flex items-center gap-4 userButton">
+            <Image
+                src="/noAvatar.png"
+                alt="No-Avatar"
+                width={36}
+                height={36}
+                className="size-9 rounded-full object-cover"
+            />
+            <Image
+                onClick={() => setOpen(prev => !prev)}
+                src="/arrow.svg"
+                alt="No-Avatar"
+                width={16}
+                height={16}
+                className="size-4 rounded-full object-cover cursor-pointer"
+            />
+
+            {open && (
+                <div
+                    className="absolute right-0 top-[120%] p-4 rounded-lg bg-white z-999 flex flex-col text-sm
+                        shadow-[0px_0px_4px_1px_rgba(0,0,0,0.177)]"
+                >
+                    <div className="cursor-pointer p-2 rounded-lg hover:bg-[#f1f1f1] text-gray-800">
+                        Profile
+                    </div>
+                    <div className="cursor-pointer p-2 rounded-lg hover:bg-[#f1f1f1] text-gray-800">
+                        Setting
+                    </div>
+                    <div className="cursor-pointer p-2 rounded-lg hover:bg-[#f1f1f1] text-gray-800">
+                        Logout
+                    </div>
+                </div>
+            )}
+        </div>
+    ) : (
+        <Link href="/" className="text-[18px] p-4 rounded-4xl hover:bg-[#f1f1f1]">
+            Login / Sign Up
+        </Link>
+    )
+
 }
 
 export default UserButton
