4064ec25ca18299ea491d5824795794d7aa1ac33
elguapo87 <pglogovac@gmail.com>
2026-06-13 00:34:40 +0200

    Created Gallery & GalleryItem components

---
+++ components/GalleryItem.tsx
@@ -0,0 +1,55 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react'
+
+type GalleryProps = {
+    id: number;
+    media: string;
+    width: number;
+    height: number;
+};
+
+const GalleryItem = ({ item }: { item: GalleryProps }) => {
+    return (
+        <div className="flex relative group" style={{ gridRowEnd: `span ${Math.ceil(item.height / 100)}` }}>
+            <Image
+                src={item.media}
+                alt="Gallery-Image"
+                width={item.width}
+                height={item.height}
+                className="w-full rounded-2xl object-cover"
+            />
+
+            <Link
+                href={`/pin/${item.id}`}
+                className="hidden group-hover:block absolute w-full h-full top-0 left-0
+                    bg-[rgba(0,0,0,0.3)] rounded-2xl"
+            />
+
+            <button
+                className="hidden group-hover:block bg-[#e50829] text-white rounded-3xl
+                    py-3 px-4 font-medium cursor-pointer w-max absolute top-4 right-4
+                    border-none text-sm"
+            >
+                Save
+            </button>
+
+            <div className="hidden group-hover:flex items-center gap-2 absolute bottom-4 right-4">
+                <button
+                    className="w-8 h-8 rounded-full bg-white flex items-center justify-center
+                        border-none cursor-pointer hover:bg-[#f1f1f1]"
+                >
+                    <Image src="/share.svg" alt="Share" width={20} height={20} className="w-5 h-5" />
+                </button>
+                <button
+                    className="w-8 h-8 rounded-full bg-white flex items-center justify-center
+                        border-none cursor-pointer hover:bg-[#f1f1f1]"
+                >
+                    <Image src="/more.svg" alt="Share" width={20} height={20} className="w-5 h-5" />
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default GalleryItem
