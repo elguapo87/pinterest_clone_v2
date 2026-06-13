ce051c1fdf35382c4ca8b9d37d052e7035f2f3a0
elguapo87 <pglogovac@gmail.com>
2026-06-14 00:44:54 +0200

    Intalled react-emoji-picker & created PostInteractions & Comments components

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,11 +1,125 @@
-import React from 'react'
+"use client"
+
+import { useState } from "react"
+import ImageKitWrapper from "./ImageKitWrapper"
+import EmojiPicker from 'emoji-picker-react'
 
 const Comments = () => {
-  return (
-    <div>
-      Comments
-    </div>
-  )
+
+    const [open, setOpen] = useState(false);
+
+    return (
+        <div className="flex-1 flex flex-col gap-4 min-h-0">
+            {/* COMMENT LIST */}
+            <div className="flex-1 flex flex-col gap-4 overflow-y-auto">
+                {/* COUNT */}
+                <span className="">5 comments</span>
+                {/* COMMENT */}
+                <div className="">
+                    <ImageKitWrapper
+                        src="/pinterest_clone/general/noAvatar.png"
+                        alt="User Avatar"
+                        width={32}
+                        height={32}
+                        imgWidth={32}
+                        className="w-8 h-8 rounded-full"
+                    />
+                    {/* COMMENT CONTENT */}
+                    <div className="">
+                        <span className="">John Doe</span>
+                        <p className="">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi vero ipsam reprehenderit
+                            cupiditate expedita necessitatibus doloremque, eaque non sit, eum provident
+                        </p>
+                        <span className="">1h ago</span>
+                    </div>
+                </div>
+                {/* COMMENT */}
+                <div className="">
+                    <ImageKitWrapper
+                        src="/general/noAvatar.png"
+                        alt="User Avatar"
+                        width={32}
+                        height={32}
+                        imgWidth={32}
+                        className="w-8 h-8 rounded-full"
+                    />
+                    {/* COMMENT CONTENT */}
+                    <div className="">
+                        <span className="">John Doe</span>
+                        <p className="">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi vero ipsam reprehenderit
+                            cupiditate expedita necessitatibus doloremque, eaque non sit, eum provident
+                        </p>
+                        <span className="">1h ago</span>
+                    </div>
+                </div>
+                {/* COMMENT */}
+                <div className="flex gap-4">
+                    <ImageKitWrapper
+                        src="/general/noAvatar.png"
+                        alt="User Avatar"
+                        width={32}
+                        height={32}
+                        imgWidth={32}
+                        className="w-8 h-8 rounded-full object-cover"
+                    />
+                    {/* COMMENT CONTENT */}
+                    <div className="flex flex-col gap-1">
+                        <span className="font-medium text-sm">John Doe</span>
+                        <p className="text-sm">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi vero ipsam reprehenderit
+                            cupiditate expedita necessitatibus doloremque, eaque non sit, eum provident
+                        </p>
+                        <span className="text-xs text-[#a6a6a6]">1h ago</span>
+                    </div>
+                </div>
+                {/* COMMENT */}
+                <div className="">
+                    <ImageKitWrapper
+                        src="/general/noAvatar.png"
+                        alt="User Avatar"
+                        width={32}
+                        height={32}
+                        imgWidth={32}
+                        className="w-8 h-8 rounded-full"
+                    />
+                    {/* COMMENT CONTENT */}
+                    <div className="">
+                        <span className="">John Doe</span>
+                        <p className="">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi vero ipsam reprehenderit
+                            cupiditate expedita necessitatibus doloremque, eaque non sit, eum provident
+                        </p>
+                        <span className="">1h ago</span>
+                    </div>
+                </div>
+            </div>
+
+            {/* COMMENT FORM */}
+            <form className="relative bg-[#f1f1f1] p-4 rounded-4xl items-center gap-4">
+                <input
+                    className="flex-1 border-none outline-none bg-transparent text-[16px]"
+                    type="text"
+                    placeholder="Add a comment"
+                />
+                {/* EMOJI */}
+                <div className="cursor-pointer text-[20px] relative">
+                    <div onClick={() => setOpen(prev => !prev)}>😀</div>
+
+                    {open && (
+                        <div
+                            className="absolute right-0 bottom-12.5 max-[751]:bottom-3
+                                max-[751px]:scale-70 max-[751px]:translate-x-[22%]"
+                        >
+                            <EmojiPicker />
+                        </div>
+                    )}
+
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Comments
--- components/PostInteractions.tsx
+++ components/PostInteractions.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import ImageKitWrapper from "./ImageKitWrapper"
 
 const PostInteractions = () => {
-  return (
-    <div>
-      PostInteractions
-    </div>
-  )
+    return (
+        <div className="flex items-center justify-between">
+            <div className="flex items-center gap-2 font-medium">
+                <ImageKitWrapper
+                    src="/pinterest_clone/general/react.svg"
+                    alt="React Icon"
+                    width={24}
+                    height={24}
+                    imgWidth={24}
+                    className="size-6 cursor-pointer"
+                />
+                273
+                <ImageKitWrapper
+                    src="/general/share.svg"
+                    alt="React Icon"
+                    width={24}
+                    height={24}
+                    imgWidth={24}
+                    className="size-6 cursor-pointer"
+                />
+                <ImageKitWrapper
+                    src="/general/more.svg"
+                    alt="React Icon"
+                    width={24}
+                    height={24}
+                    imgWidth={24}
+                    className="size-6 cursor-pointer"
+                />
+            </div>
+            <button
+                className="bg-[#e50829] text-white border-none rounded-3xl px-4 py-3
+                    text-sm font-bold cursor-pointer"
+            >
+                Save
+            </button>
+        </div>
+    )
 }
 
 export default PostInteractions
