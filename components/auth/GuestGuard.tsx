aa47675abc02dc4dac11853df82ec8cbdd1f4f2c
elguapo87 <pglogovac@gmail.com>
2026-06-17 01:26:20 +0200

    Created GuestGuard component

---
+++ components/auth/GuestGuard.tsx
@@ -0,0 +1,22 @@
+import { AuthContext } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect } from "react";
+import Loader from "../Loader";
+
+export default function GuestGuard({ children }: { children: React.ReactNode }) {
+    const authContext = useContext(AuthContext);
+    if (!authContext) throw new Error("AuthContext must be within AuthContextProvider");
+    const { user, loading } = authContext;
+
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!loading && user) {
+            router.replace("/");
+        }
+    }, [user, loading, router]);
+
+    if (loading) return <Loader />
+
+    return <>{children}</>
+}
