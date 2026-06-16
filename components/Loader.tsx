6c1bcb5d1e51c8b1e4e487a7da2f0673ae5dc93a
elguapo87 <pglogovac@gmail.com>
2026-06-17 01:14:35 +0200

    Created Loader (spinning) component

---
+++ components/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+    return (
+        <div className='flex items-center justify-center h-full'>
+            <div className='size-12 border-3 border-blue-500 border-t-transparent rounded-full animate-spin' />
+        </div>
+    )
+}
+
+export default Loader
