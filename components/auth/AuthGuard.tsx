ec59683adfbda2b5c7b8bf62d3bfb07386dea684
elguapo87 <pglogovac@gmail.com>
2026-06-17 13:42:43 +0200

    Created AuthGuard component

---
+++ components/auth/AuthGuard.tsx
@@ -0,0 +1,22 @@
+import { AuthContext } from "@/context/AuthContext";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect } from "react";
+import Loader from "../Loader";
+
+export default function AuthGuard({ children }: { children: React.ReactNode }) {
+    const authContext = useContext(AuthContext);
+    if (!authContext) throw new Error("AuthGuard must be within AuthContextProvider");
+    const { user, loading } = authContext;
+
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!loading && !user) {
+            router.replace("/auth");
+        }
+    }, [user, loading, router]);
+
+    if (loading && !user) return <Loader />
+
+    return <>{children}</>
+}
