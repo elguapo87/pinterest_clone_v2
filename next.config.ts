8766eaf1729363db93981b25be262afa44dd53a9
elguapo87 <pglogovac@gmail.com>
2026-07-15 20:14:48 +0200

    Changed next.config.ts for napi-rs/canvas

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["@napi-rs/canvas"],
+
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'ik.imagekit.io',
-        port: '',
-        pathname: '/**'
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
